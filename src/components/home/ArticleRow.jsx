e682d3a0891837619a07eeeb53895866850ca3af
poodlepoodle <chammal97@naver.com>
2024-02-09 00:30:51 +0900

    feat(ArticleRow.jsx): 미리보기 텍스트 상단 정렬 조정

--- src/components/home/ArticleRow.jsx
+++ src/components/home/ArticleRow.jsx
@@ -1,15 +1,12 @@
 import styled from 'styled-components';
 
-import thumbs from '@assets/home/sample_thumbnail.png';
-
 const Container = styled.div`
   width: 100%;
   height: 5.88rem;
-  border-top: 0.03125rem solid #a9a9a9;
-  padding: 0 1.38rem;
+  padding: 0.75rem 1.38rem;
 
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   column-gap: 1rem;
 `;
 
@@ -17,29 +14,31 @@ const Thumbnail = styled.div`
   min-width: 4.375rem;
   max-width: 4.375rem;
   height: 4.375rem;
+  flex-shrink: 0;
+
   border-radius: 0.5rem;
   overflow: hidden;
 
   img {
     width: 100%;
     height: 100%;
+    object-fit: cover;
   }
 `;
 
 const Description = styled.p`
-  color: #333;
+  /* height: 100%; */
+  flex-grow: 1;
+  margin-top: 0.5rem;
 
-  font-family: AppleSDGothicNeo;
+  color: #333;
+  font-family: 'Apple SD Gothic Neo';
   font-size: 0.875rem;
   font-style: normal;
   font-weight: 400;
   line-height: normal;
 
   text-overflow: ellipsis;
-
-  /* Needed to make it work */
-  /* overflow: hidden;
-  white-space: nowrap; */
 
   display: -webkit-box;
   -webkit-line-clamp: 3;
@@ -52,14 +51,10 @@ const ArticleRow = (props) => {
     <Container>
       {props.thumbnail && (
         <Thumbnail>
-          <img src={thumbs} />
+          <img src={props.thumbnail} />
         </Thumbnail>
       )}
-      <Description>
-        봄은 자연에서 새로운 생명과 활기가 물씬 풍기는 아름다운 계절입니다.
-        새로운 잎이 나고 꽃들이 피어나며 새로운 생명이 시작되는 시기이죠. 봄은
-        이러한 자연의 신생을 상징하면서 새로운 생명과
-      </Description>
+      <Description>{props.text}</Description>
     </Container>
   );
 };
