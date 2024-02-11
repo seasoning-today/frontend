54108f276daaa4744aeb23d9faaeb80b243401c2
poodlepoodle <chammal97@naver.com>
2024-02-11 17:21:31 +0900

    chore(main.jsx): Noto Serif KR 폰트 link로 로딩하도록 변경

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,16 @@
 import './index.css';
-import '@assets/fonts/notoserifkr/NotoSerifKR.css';
+// import '@assets/fonts/notoserifkr/NotoSerifKR.css';
 import '@assets/fonts/applesdgothicneo/AppleSDGothicNeo.css';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-// import { RecoilRoot } from 'recoil';
 
 import { RouterInfo } from './utils/router/RouterInfo';
 
 const router = createBrowserRouter(RouterInfo);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <RecoilRoot>
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-  // </RecoilRoot>
 );
