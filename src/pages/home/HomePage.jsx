fc047286a7d797b60d6cfa701e6c4a222e356d20
poodlepoodle <chammal97@naver.com>
2024-02-21 21:29:54 +0900

    fix(HomePage.jsx): 연도 및 절기 select 너비 패딩 적용

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -147,13 +147,14 @@ const Select = styled.div`
   align-items: center;
 
   select {
-    width: 5.7rem;
-    height: 1.25rem;
-    text-align: center;
+    height: 1.5rem;
+
+    padding: 0 0.38rem;
 
     border: none;
     outline: none;
 
+    text-align: center;
     color: #333;
     font-family: 'Apple SD Gothic Neo';
     font-size: 0.875rem;
@@ -179,7 +180,7 @@ const ContentArea = styled.div`
 
 const HomePage = () => {
   const { homeData, termData, newNotificationData } = useLoaderData();
-  console.log(JSON.stringify(homeData, null, '\t'));
+  // console.log(JSON.stringify(homeData, null, '\t'));
 
   const [now, setNow] = useState(new Date());
 
@@ -313,15 +314,35 @@ const HomePage = () => {
           </select>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
+            width="9"
+            height="5"
+            viewBox="0 0 9 5"
             fill="none"
           >
-            <path
-              d="M12.0005 14.6538L7.59668 10.25H16.4043L12.0005 14.6538Z"
-              fill="black"
-            />
+            <mask
+              id="mask0_2024_1182"
+              maskUnits="userSpaceOnUse"
+              x="0"
+              y="0"
+              width="9"
+              height="5"
+            >
+              <mask id="path-1-inside-1_2024_1182" fill="white">
+                <path d="M8.3871 0L4.19355 5L0 0H8.3871Z" />
+              </mask>
+              <path d="M8.3871 0L4.19355 5L0 0H8.3871Z" fill="#02211D" />
+              <path
+                d="M8.3871 0L11.4519 2.57045C12.4507 1.37951 12.6692 -0.282235 12.0121 -1.69089C11.3551 -3.09954 9.94146 -4 8.3871 -4V0ZM4.19355 5L1.12878 7.57045C1.88879 8.47662 3.01086 9 4.19355 9C5.37624 9 6.4983 8.47662 7.25831 7.57045L4.19355 5ZM0 0V-4C-1.55436 -4 -2.96798 -3.09954 -3.62504 -1.69089C-4.2821 -0.282235 -4.06362 1.37951 -3.06477 2.57045L0 0ZM5.32233 -2.57045L1.12878 2.42955L7.25831 7.57045L11.4519 2.57045L5.32233 -2.57045ZM7.25831 2.42955L3.06477 -2.57045L-3.06477 2.57045L1.12878 7.57045L7.25831 2.42955ZM0 4H8.3871V-4H0V4Z"
+                fill="white"
+                mask="url(#path-1-inside-1_2024_1182)"
+              />
+            </mask>
+            <g mask="url(#mask0_2024_1182)">
+              <path
+                d="M-4.19336 -7.91602H12.5808V12.084H-4.19336V-7.91602Z"
+                fill="#02211D"
+              />
+            </g>
           </svg>
         </Select>
       </Category>
