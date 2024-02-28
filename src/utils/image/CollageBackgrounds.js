a118501f2b4aec72f81278e2c9095a69eae6f3f9
poodlepoodle <chammal97@naver.com>
2024-02-28 21:57:49 +0900

    refactor(@assets/collage): 콜라주 페이지 이미지 에셋 압축

---
+++ src/utils/image/CollageBackgrounds.js
@@ -0,0 +1,51 @@
+import collage_background_1 from '@assets/collage/1.webp';
+import collage_background_2 from '@assets/collage/2.webp';
+import collage_background_3 from '@assets/collage/3.webp';
+import collage_background_4 from '@assets/collage/4.webp';
+import collage_background_5 from '@assets/collage/5.webp';
+import collage_background_6 from '@assets/collage/6.webp';
+import collage_background_7 from '@assets/collage/7.webp';
+import collage_background_8 from '@assets/collage/8.webp';
+import collage_background_9 from '@assets/collage/9.webp';
+import collage_background_10 from '@assets/collage/10.webp';
+import collage_background_11 from '@assets/collage/11.webp';
+import collage_background_12 from '@assets/collage/12.webp';
+import collage_background_13 from '@assets/collage/13.webp';
+import collage_background_14 from '@assets/collage/14.webp';
+import collage_background_15 from '@assets/collage/15.webp';
+import collage_background_16 from '@assets/collage/16.webp';
+import collage_background_17 from '@assets/collage/17.webp';
+import collage_background_18 from '@assets/collage/18.webp';
+import collage_background_19 from '@assets/collage/19.webp';
+import collage_background_20 from '@assets/collage/20.webp';
+import collage_background_21 from '@assets/collage/21.webp';
+import collage_background_22 from '@assets/collage/22.webp';
+import collage_background_23 from '@assets/collage/23.webp';
+import collage_background_24 from '@assets/collage/24.webp';
+
+export const CollageBackgrounds = {
+  1: collage_background_1,
+  2: collage_background_2,
+  3: collage_background_3,
+  4: collage_background_4,
+  5: collage_background_5,
+  6: collage_background_6,
+  7: collage_background_7,
+  8: collage_background_8,
+  9: collage_background_9,
+  10: collage_background_10,
+  11: collage_background_11,
+  12: collage_background_12,
+  13: collage_background_13,
+  14: collage_background_14,
+  15: collage_background_15,
+  16: collage_background_16,
+  17: collage_background_17,
+  18: collage_background_18,
+  19: collage_background_19,
+  20: collage_background_20,
+  21: collage_background_21,
+  22: collage_background_22,
+  23: collage_background_23,
+  24: collage_background_24,
+};
