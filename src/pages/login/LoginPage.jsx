0164307936b5ec4b2133eb9feb1a21613442ceda
sem-git <lsmsm0327@naver.com>
2024-03-31 01:46:51 +0900

    feat: 온보딩 이미지 assets에 추가

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import ImageSlider from '@components/write/ImageSlider';
 import kakao_btn from '@assets/login/kakao_btn.webp';
 import login_background from '@assets/login/login-background.webp';
 
@@ -38,8 +39,8 @@ const ButtonRow = styled.div`
 `;
 
 function LoginPage() {
-  const KAKAO_REST_API_KEY = `c574e4572cdf6171c9cb1fe3af45bf75`; // 후에 환경변수 설정 예정
-  const REDIRECT_URI = 'https://seasoning.today/callback/kakao/login'; // 후에 환경변수 설정 예정
+  const KAKAO_REST_API_KEY = `c574e4572cdf6171c9cb1fe3af45bf75`;
+  const REDIRECT_URI = 'https://seasoning.today/callback/kakao/login';
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
 
   const handleKakaoLogin = () => {
