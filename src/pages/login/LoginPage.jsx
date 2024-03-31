9a4e28781359fd42f2925ec901db2fd8f7da96dd
sem-git <lsmsm0327@naver.com>
2024-04-01 01:51:22 +0900

    feat: 온보딩 이미지 스크롤 구현 및 인디케이터 추가

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,7 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components';
-
-import ImageSlider from '@components/write/ImageSlider';
 
 import kakao_btn from '@assets/login/kakao_btn.webp';
 import login_background from '@assets/login/login-background.webp';
@@ -16,14 +14,9 @@ const Background = styled.div`
   height: 100%;
 
   display: flex;
-  justify-content: center;
   align-items: center;
-
-  background-image: url(${login_background});
-  /* background-repeat: no-repeat; */
-  /* background-position: top center; */
-  background-size: cover;
-  /* background-attachment: fixed; */
+  flex-direction: column;
+  gap: 1.5rem;
 
   overflow: hidden;
 `;
@@ -31,13 +24,52 @@ const Background = styled.div`
 const OnboardingContainer = styled.div`
   position: relative;
   width: 100%;
+  height: 37rem;
+  flex-shrink: 0;
+
+  display: flex;
+  align-items: center;
+  margin-top: 3rem;
+  overflow-x: auto;
+  column-gap: 1.5rem;
+`;
+
+const OnboardingImages = styled.img`
+  width: 100%;
+  height: 36.6875rem;
+  flex-shrink: 0;
+`;
+
+const DotsContainer = styled.div`
+  width: 100%;
+  height: 1rem;
+  flex-shrink: 0;
+  column-gap: 0.5rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Dots = styled.div`
+  display: flex;
+  flex-shrink: 0;
+
+  width: 0.4375rem;
+  height: 0.4375rem;
+  border-radius: 0.5rem;
+  background-color: ${({ active }) =>
+    active ? '#195A53;' : 'var(--D9, #D9D9D9)'};
+
+  transition: all 0.2s ease-in-out;
+  cursor: pointer;
 `;
 
 const ButtonRow = styled.div`
   position: absolute;
   bottom: 0;
   width: 100%;
-  margin-bottom: 6.38rem;
+  margin-bottom: 1rem;
 
   display: flex;
   justify-content: center;
@@ -53,9 +85,18 @@ const ButtonRow = styled.div`
 `;
 
 function LoginPage() {
+  /* 로그인 */
   const KAKAO_REST_API_KEY = `c574e4572cdf6171c9cb1fe3af45bf75`;
   const REDIRECT_URI = 'https://seasoning.today/callback/kakao/login';
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
+
+  const handleKakaoLogin = () => {
+    window.location.href = KAKAO_AUTH_URL;
+  };
+
+  /* 온보딩 */
+  const [activeDotIndex, setActiveDotIndex] = useState(0);
+  const imageScrollRef = useRef();
 
   const onboardingImageURLs = [
     onboarding1,
@@ -64,17 +105,40 @@ function LoginPage() {
     onboarding4,
   ];
 
-  const [onboardingImages, setOnboardingImages] = useState(onboardingImageURLs);
+  const handleDotClick = (index) => {
+    const imageWidth = imageScrollRef.current.firstChild.offsetWidth;
+    imageScrollRef.current.scrollTo({
+      left: index * imageWidth,
+      behavior: 'smooth',
+    });
+  };
 
-  const handleKakaoLogin = () => {
-    window.location.href = KAKAO_AUTH_URL;
+  const handleImageScroll = () => {
+    const containerWidth = imageScrollRef.current.offsetWidth;
+    const scrollLeft = imageScrollRef.current.scrollLeft + containerWidth / 2;
+    const index = Math.floor(scrollLeft / containerWidth);
+
+    setActiveDotIndex(index);
   };
 
   return (
     <Background>
-      <OnboardingContainer>
-        <ImageSlider images={onboardingImages} />
+      <OnboardingContainer ref={imageScrollRef} onScroll={handleImageScroll}>
+        {onboardingImageURLs.map((imageURL, index) => (
+          <OnboardingImages key={index} src={imageURL} />
+        ))}
       </OnboardingContainer>
+
+      <DotsContainer>
+        {onboardingImageURLs.map((_, index) => (
+          <Dots
+            key={index}
+            onClick={() => handleDotClick(index)}
+            active={index === activeDotIndex}
+          />
+        ))}
+      </DotsContainer>
+
       <ButtonRow onClick={handleKakaoLogin}>
         <img src={kakao_btn} />
       </ButtonRow>
