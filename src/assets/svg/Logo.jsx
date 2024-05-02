b70c603b33270026b95a8c98f76af2bce6e2d5c1
poodlepoodle <chammal97@naver.com>
2024-05-02 16:56:57 +0900

    feat(assets/svg): 로고 파일 추가

---
+++ src/assets/svg/Logo.jsx
@@ -0,0 +1,46 @@
+export default function Logo() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="159"
+      height="277"
+      viewBox="0 0 159 277"
+      fill="none"
+    >
+      <path
+        d="M158 209.864C158 246.661 128.239 276.5 91.5019 276.5H1C5.84111 267.53 95.99 96.7728 106.042 77.7473C102.235 75.5997 98.1248 73.7552 93.7292 72.273C66.3466 63.0339 38.9473 69.9485 32.795 89.4877C28.6431 102.685 35.5266 120.245 50.8063 132.221C28.9373 120.397 19.5744 103.881 19.5744 73.132C19.5744 33.0261 52.0334 0.5 92.0566 0.5C109.614 0.5 128.676 6.84183 143.535 22.0184C137.232 33.7504 45.1079 208.298 40.1828 217.638H92.1911C112.69 217.638 129.323 200.979 129.323 180.429C129.323 164.082 123.549 154.211 105.252 144.829C136.929 151.415 158 178.046 158 209.856V209.864Z"
+        fill="url(#paint0_linear_669_2925)"
+        stroke="url(#paint1_linear_669_2925)"
+        strokeWidth="0.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_669_2925"
+          x1="79.5"
+          y1="0.5"
+          x2="79.5"
+          y2="276.5"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#959595" />
+          <stop offset="0.479167" stopColor="#EDEDED" stopOpacity="0.92" />
+          <stop offset="1" stopColor="white" stopOpacity="0.6" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_669_2925"
+          x1="79.5"
+          y1="0.5"
+          x2="79.5"
+          y2="276.5"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#959595" />
+          <stop offset="0.479167" stopColor="#EDEDED" stopOpacity="0.92" />
+          <stop offset="1" stopColor="white" stopOpacity="0.6" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
