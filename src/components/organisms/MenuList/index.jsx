8e63010faabbd59abc9b7059796a357c71ad04f8
poodlepoodle <chammal97@naver.com>
2024-05-02 16:43:31 +0900

    feat(organisms/MenuList): MenuListItem에 전달하는 props에 value 추가

--- src/components/organisms/MenuList/index.jsx
+++ src/components/organisms/MenuList/index.jsx
@@ -1,5 +1,7 @@
 import * as S from './style';
 
+import { Fragment } from 'react';
+
 import Divider from '@components/atoms/Divider';
 
 import MenuListItem from '@components/molecules/MenuListItem';
@@ -7,13 +9,13 @@ import MenuListItem from '@components/molecules/MenuListItem';
 export default function MenuList({ menuListData }) {
   return (
     <S.Layout>
-      {menuListData.map(({ type, text, to, action }, index) => (
-        <>
-          {index > 0 && <Divider borderWidth="0.03125" margin={1.125} />}
-          <MenuListItem key={index} type={type} to={to} action={action}>
+      {menuListData.map(({ type, text, to, value, action }, index) => (
+        <Fragment key={index}>
+          {index > 0 && <Divider borderWidth="0.03125" margin="1.125" />}
+          <MenuListItem type={type} to={to} value={value} action={action}>
             {text}
           </MenuListItem>
-        </>
+        </Fragment>
       ))}
     </S.Layout>
   );
