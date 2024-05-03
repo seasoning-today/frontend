70f6092ecd18de326988f940714a1f2aabc75ea5
poodlepoodle <chammal97@naver.com>
2024-05-03 21:10:24 +0900

    refactor(molecules/NoticeListItem): 아토믹 디자인 기반 리팩토링

---
+++ src/components/molecules/NoticeListItem/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  row-gap: 0.75rem;
+  padding: 0 0.63rem;
+`;
