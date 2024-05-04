ce7054e811bfdfde9a84ccde0d2ce1768501403c
poodlepoodle <chammal97@naver.com>
2024-05-04 22:26:27 +0900

    feat(atoms/Image): circle props 추가

--- src/components/atoms/Image/index.jsx
+++ src/components/atoms/Image/index.jsx
@@ -4,11 +4,18 @@ export default function Image({
   width = 1,
   height = 1,
   radius = 0,
+  circle = false,
   src,
   ...props
 }) {
   return (
-    <S.Layout width={width} height={height} radius={radius} {...props}>
+    <S.Layout
+      width={width}
+      height={height}
+      radius={radius}
+      circle={circle}
+      {...props}
+    >
       <S.Image src={src} />
     </S.Layout>
   );
