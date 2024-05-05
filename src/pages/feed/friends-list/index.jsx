2d4af00d9883a6e64bab0ac78a54ab61fde2ea56
poodlepoodle <chammal97@naver.com>
2024-05-05 20:52:37 +0900

    refactor(pages/FriendsListPage): 아토믹 디자인 기반 리팩토링

--- src/pages/feed/friends-list/index.jsx
+++ src/pages/feed/friends-list/index.jsx
@@ -2,10 +2,8 @@ import { useLoaderData } from 'react-router-dom';
 
 import FriendsListTemplate from '@components/templates/FriendsListTemplate';
 
-const FriendsListPage = () => {
+export default function FriendsListPage() {
   const { friendListData } = useLoaderData();
 
   return <FriendsListTemplate friendListData={friendListData} />;
-};
-
-export default FriendsListPage;
+}
