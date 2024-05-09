5f224ea6cc099dea6a239e25af850c4344c2c385
poodlepoodle <chammal97@naver.com>
2024-05-10 01:15:52 +0900

    refactor(templates/SeasonTemplate): 아토믹 디자인 기반 리팩토링

---
+++ src/components/templates/HomeTemplate/Season/index.jsx
@@ -0,0 +1,29 @@
+import * as S from './style';
+
+import Text from '@components/atoms/Text';
+import SeasonFeedListItem from '@components/organisms/SeasonFeedListItem';
+
+export default function SeasonTemplate({ homeData }) {
+  return (
+    <S.Layout>
+      {homeData.length > 0 ? (
+        homeData.map(({ id, term, year, image, preview }) => (
+          <SeasonFeedListItem
+            key={id}
+            articleId={id}
+            term={term}
+            year={year}
+            image={image}
+            preview={preview}
+          />
+        ))
+      ) : (
+        <S.EmptyContent>
+          <Text size="1" color="#8c8c8c">
+            해당 절기에 대한 기록이 없습니다.
+          </Text>
+        </S.EmptyContent>
+      )}
+    </S.Layout>
+  );
+}
---
+++ src/components/templates/HomeTemplate/Season/style.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Layout = styled.ul`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 1.5rem;
+  padding: 0.5rem 1.31rem 1.5rem;
+`;
+
+export const EmptyContent = styled.li`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 1rem;
+`;
