b0667c6e147cd3184b4911dd341fc37f62a1673d
poodlepoodle <chammal97@naver.com>
2024-05-16 12:39:10 +0900

    refactor(organisms/OnboardingCarousel): 아토믹 디자인 기반 컴포넌트 분리

---
+++ src/components/organisms/OnboardingCarousel/style.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+export const Layout = styled.ul`
+  position: relative;
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Carousel = styled.ul`
+  position: relative;
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  align-items: center;
+  column-gap: 1.5rem;
+  padding-bottom: 1rem;
+  overflow-x: auto;
+  scroll-snap-type: x mandatory;
+`;
+
+export const Indicator = styled.ul`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  z-index: 11;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  column-gap: 0.5rem;
+`;
+
+export const IndicatorItem = styled.li`
+  position: relative;
+  width: ${({ active }) => (active ? '0.5rem' : '0.4375rem')};
+  height: ${({ active }) => (active ? '0.5rem' : '0.4375rem')};
+  border-radius: 50%;
+
+  background-color: ${({ active }) => (active ? '#195A53' : '#d9d9d9')};
+
+  transition: all 0.2s ease-in-out;
+`;
