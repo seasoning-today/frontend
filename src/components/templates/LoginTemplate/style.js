a466eba5a5e7106d5b775df9efa7b95f667e2d96
poodlepoodle <chammal97@naver.com>
2024-05-16 12:40:07 +0900

    refactor(templates/LoginTemplate): organisms/OnboardingCarousel 적용

---
+++ src/components/templates/LoginTemplate/style.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Layout = styled.div`
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 1rem;
+`;
+
+export const OnboardingCarouselContainer = styled.div`
+  width: 100%;
+
+  padding: 0 2rem;
+`;
+
+export const ButtonContainer = styled.div`
+  position: relative;
+  width: 100%;
+  flex-shrink: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
