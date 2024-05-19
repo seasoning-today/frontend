34de5903c4be1b7b82eecd2de447f5d7a937f9ab
poodlepoodle <chammal97@naver.com>
2024-05-19 16:47:06 +0900

    feat(pages/MyPage): 절기 알림 설정 링크 메뉴 추가

--- src/pages/mypage/index.jsx
+++ src/pages/mypage/index.jsx
@@ -16,6 +16,7 @@ export default function MyPage() {
       { type: 'link', text: '프로필 수정', to: `/mypage/edit` },
       { type: 'link', text: '계정 설정', to: `/mypage/account` },
     ],
+    [{ type: 'link', text: '절기 알림 설정', to: `/mypage/kakao-friends` }],
     [{ type: 'link', text: '공지사항', to: `/notice` }],
     [
       {
