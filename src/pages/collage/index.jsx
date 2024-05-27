5f380775029e682dfb77d56437a3e3a19ca153f4
poodlepoodle <chammal97@naver.com>
2024-05-27 16:34:01 +0900

    refactor(pages/CollagePage): 상태를 모두 Page에서 관리하도록 변경

--- src/pages/collage/index.jsx
+++ src/pages/collage/index.jsx
@@ -1,14 +1,48 @@
-import { useLoaderData } from 'react-router-dom';
-
 import CollageTemplate from '@components/templates/CollageTemplate';
 
+import { useEffect, useState } from 'react';
+import { useLoaderData, useLocation, useNavigate } from 'react-router-dom';
+
 const CollagePage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
   const { collageData, newNotificationData } = useLoaderData();
+  const [selectedCategory, setSelectedCategory] = useState(2024);
+  const [imageEnabled, setImageEnabled] = useState(true);
+  const [labelEnabled, setLabelEnabled] = useState(false);
+
+  const handleCategoryChange = (event) => {
+    const newCategory = event.target.value;
+    setSelectedCategory(newCategory);
+    navigate(`/collage?category=${newCategory}`);
+  };
+
+  const toggleImageEnabled = () => {
+    setImageEnabled((imageEnabled) => !imageEnabled);
+  };
+
+  const toggleLabelEnabled = () => {
+    setLabelEnabled((labelEnabled) => !labelEnabled);
+  };
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const categoryParam = searchParams.get('category');
+    if (categoryParam) {
+      setSelectedCategory(categoryParam);
+    }
+  }, [location.search]);
 
   return (
     <CollageTemplate
-      collageData={collageData}
       isNewNotification={newNotificationData}
+      collageData={collageData}
+      selectedCategory={selectedCategory}
+      handleCategoryChange={handleCategoryChange}
+      imageEnabled={imageEnabled}
+      toggleImageEnabled={toggleImageEnabled}
+      labelEnabled={labelEnabled}
+      toggleLabelEnabled={toggleLabelEnabled}
     />
   );
 };
