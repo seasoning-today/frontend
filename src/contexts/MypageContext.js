33d499f2d95acb7165c38a0b3a7846a337eb64a2
poodlepoodle <chammal97@naver.com>
2024-05-30 12:19:12 +0900

    feat(contexts/MypageContext): MyPage 전역 상태관리 로직 분리

---
+++ src/contexts/MypageContext.js
@@ -0,0 +1,32 @@
+import { createContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export const MypageContext = createContext();
+
+export function useMypageContext(loaderData) {
+  const navigate = useNavigate();
+  const { userData } = loaderData;
+
+  const handleClickLogout = () => {
+    localStorage.clear();
+    navigate(`/login`);
+  };
+
+  const menuListsData = [
+    [
+      { type: 'link', text: '프로필 수정', to: `/mypage/edit` },
+      { type: 'link', text: '계정 설정', to: `/mypage/account` },
+    ],
+    [{ type: 'link', text: '절기 알림 설정', to: `/mypage/kakao-friends` }],
+    [{ type: 'link', text: '공지사항', to: `/notice` }],
+    [
+      {
+        type: 'action',
+        text: '로그아웃',
+        action: handleClickLogout,
+      },
+    ],
+  ];
+
+  return { userData, menuListsData };
+}
