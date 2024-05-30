d24e2e37a15868b7c1f68fcc616bf34f46a4ff0d
poodlepoodle <chammal97@naver.com>
2024-05-31 01:40:13 +0900

    refactor: SearchContext 전역 상태 적용

--- src/pages/feed/friends-search/index.jsx
+++ src/pages/feed/friends-search/index.jsx
@@ -1,5 +1,13 @@
 import SearchTemplate from '@components/templates/SearchTemplate';
 
+import { SearchContext, useSearchContext } from '@contexts/SearchContext';
+
 export default function SearchPage() {
-  return <SearchTemplate />;
+  const searchContextValue = useSearchContext();
+
+  return (
+    <SearchContext.Provider value={searchContextValue}>
+      <SearchTemplate />
+    </SearchContext.Provider>
+  );
 }
