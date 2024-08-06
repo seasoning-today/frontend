500e3dc59da1ea72bde0f9287aff694dc59c255e
sem-git <lsmsm0327@naver.com>
2024-08-06 18:55:03 +0900

    refactor(atoms/Divider): typescript 적용

--- src/components/atoms/Divider/index.tsx
+++ src/components/atoms/Divider/index.tsx
@@ -1,11 +1,17 @@
 import * as S from './style';
 
+interface DividerProps {
+  borderWidth?: number;
+  margin?: number;
+  color?: string;
+}
+
 export default function Divider({
   borderWidth = 1,
   margin = 0,
   color = '#f0f0f0',
   ...props
-}) {
+}: DividerProps) {
   return (
     <S.Layout
       borderWidth={borderWidth}
--- src/components/atoms/Divider/style.ts
+++ src/components/atoms/Divider/style.ts
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 
-export const Layout = styled.hr`
+interface LayoutProps {
+  margin?: number;
+  borderWidth?: number;
+  color?: string;
+}
+
+export const Layout = styled.hr<LayoutProps>`
   width: ${({ margin }) => `calc(100% - ${margin}rem * 2)`};
   border-width: ${({ borderWidth }) => `${borderWidth}rem 0 0 0`};
 
